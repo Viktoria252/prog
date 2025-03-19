f5787f65c72f15a379ef5a0fcd29a6fcd95ca684
Viktoria <kimviktoria25@mail.ru>
2025-03-19 07:53:26 +0300

    updated 1

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Это добавлено в ветке main"); }
+        System.out.print("Это добавлено в ветке main");
+        String a = "Первое изменение в Main";
+        System.out.println(a);
+    }
 }
